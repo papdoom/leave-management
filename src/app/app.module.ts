7237f62169684049ce7c5cffc94dcf2fd7b4879e
CYBILLTEK\dominick.caacbay <ddm.caacbay@gmail.com>
2019-05-20 18:16:21 +0800

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AdminComponent } from './admin/admin.component';
+import { UserComponent } from './user/user.component';
+import { HistoryComponent } from './history/history.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    AdminComponent,
+    UserComponent,
+    HistoryComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
